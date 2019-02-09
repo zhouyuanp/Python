a8481f88b87c5945e09f4c920ab733e571a8b29e
zhouyuanp <zyp112490@163.com>
2019-02-09 16:46:34 +0800

    classStudent1 实例属性内存分析

--- Object/classStudent1.py
+++ Object/classStudent1.py
@@ -4,11 +4,23 @@ class Student1:
 
         self.name = name
         self.score = score
-    def say_score(self):
+    def say_score(self):  #方法也是属性
         print("{0}的分数是：{1}".format(self.name,self.score))
-
 
 s2 = Student1("张三",18)
 s2.say_score()
+
+s2.age = 30
+s2.salary = 3000  #新增的实例属性
+print(s2.salary)
+
+
+s1 = Student1("zyp",18)  #新增的实例属性，s1中调用不了
+s1.say_score()
+
+
+
+
+
 
 

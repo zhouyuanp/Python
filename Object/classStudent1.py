390cfb66ac06f957b87986eea3479b594e9c3c04
zhouyuanp <zyp112490@163.com>
2019-02-11 08:51:52 +0800

    classStudent1 实例方法内存分析

--- Object/classStudent1.py
+++ Object/classStudent1.py
@@ -17,6 +17,26 @@ print(s2.salary)
 
 s1 = Student1("zyp",18)  #新增的实例属性，s1中调用不了
 s1.say_score()
+Student1.say_score(s1)    #解释器调用
+#查看对象s1属性
+print(dir(s1))
+print(dir(Student1))
+#查看编译时的属性
+print(s1.__dict__)
+print(Student1.__dict__)
+#空语句
+class Man:
+    pass   # 先定义一个类，还想好怎么写
+
+#判断对象是不是某个类的实例对象
+print(isinstance(s1,Man))
+print(isinstance(s1,Student1))
+
+
+
+
+
+
 
 
 

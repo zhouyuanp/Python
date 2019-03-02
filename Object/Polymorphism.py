7bdd91005bdc84de19a9d0f6f45503d1c85478d2
zhouyuanp <zyp112490@163.com>
2019-03-02 13:57:00 +0800

    多态

--- Object/Polymorphism.py
+++ Object/Polymorphism.py
@@ -20,7 +20,7 @@ def manEat(m):
     if isinstance(m,Man):
         m.eat()   #是Man的子类就可以  #多态 一个方法的调用 根据对象的不同,调用不同的方法
     else:
-        print("不能吃饭,要自信")
+        print("不能吃太多饭,要减肥")
 
 manEat(Man())
 manEat(Chinese())
